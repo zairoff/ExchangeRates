485892863388bfa5c6b1547f019be520073aa1c7
zairoff <marufzoiroff@gmail.com>
2023-12-02 16:05:11 +0500

    create controller and register services

---
+++ ExchangeRates.Api/Controllers/CurrencyController.cs
@@ -0,0 +1,93 @@
+﻿using ExchangeRates.Abstractions.Services;
+using ExchangeRates.Common;
+using ExchangeRates.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRates.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyService _financialService;
+        private readonly ILogger<CurrencyController> _logger;
+
+        public CurrencyController(ICurrencyService financialService, ILogger<CurrencyController> logger)
+        {
+            _financialService = financialService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var currencies = await _financialService.GetAsync();
+
+                return new OkObjectResult(currencies);
+            }
+            catch (ExchangeRatesException ex)
+            {
+                _logger.LogWarning(ex, ex.Message);
+
+                return new BadRequestObjectResult(ex.ExternalMessage);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            
+        }
+
+        [HttpGet]
+        [Route("Latest")]
+        public async Task<IActionResult> GetLatest()
+        {
+            try
+            {
+                var currencies = await _financialService.GetLatestAsync();
+
+                return new OkObjectResult(currencies);
+            }
+            catch (ExchangeRatesException ex)
+            {
+                _logger.LogWarning(ex, ex.Message);
+
+                return new BadRequestObjectResult(ex.ExternalMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("Convert")]
+        public async Task<IActionResult> Convert([FromBody] ConvertArgs args)
+        {
+            try
+            {
+                var currencies = await _financialService.ConvertAsync(args.Value, args.From, args.To);
+
+                return new OkObjectResult(currencies);
+            }
+            catch (ExchangeRatesException ex)
+            {
+                _logger.LogWarning(ex, ex.Message);
+
+                return new BadRequestObjectResult(ex.ExternalMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
--- ExchangeRates.Api/Program.cs
+++ ExchangeRates.Api/Program.cs
@@ -1,3 +1,6 @@
+using ExchangeRates.Abstractions.Services;
+using ExchangeRates.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+RegisterServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
@@ -23,3 +28,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void RegisterServices(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddHttpClient<ICurrencyService, CurrencyService>();
+}
