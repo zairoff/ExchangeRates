0d3851adb15546cb818a3418c91dc73d97f37816
zairoff <marufzoiroff@gmail.com>
2023-12-02 16:04:46 +0500

    abstraction of service

---
+++ ExchangeRates.Abstractions/Services/ICurrencyService.cs
@@ -0,0 +1,13 @@
+﻿using ExchangeRates.Contracts;
+
+namespace ExchangeRates.Abstractions.Services
+{
+    public interface ICurrencyService
+    {
+        Task<IReadOnlyCollection<Currency>> GetAsync();
+
+        Task<IReadOnlyCollection<Currency>> GetLatestAsync();
+
+        Task<double> ConvertAsync(double value, string from, string to);
+    }
+}
